06e46f1f2cfa2e0f83654148eec5713a08ce176b
harodggg <haroldtiansheng@gmail.com>
2023-03-31 17:31:26 +0800

    add requrements.txt

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Gmail server
+gmail_server = "smtp.gmail.com"
 
+# Gmail account
+gmail_user = "haroldtiansheng@gmail.com"
+passwd = "password":
+
+# Outlook server
+outlook_server = "smtp-mail.outlook.com"
+
+# Outlook account
+account_path = "./3000.csv"
+from_account =  "0"
+to_account = "1"
